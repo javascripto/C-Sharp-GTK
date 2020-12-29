260b125c15916f01015568e86cf37769c07329b6
Yuri Almeida <yurialves2@gmail.com>
2020-12-29 12:45:23 -0300

    init

---
+++ C-Sharp-GTK2/MainWindow.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using Gtk;
+
+public partial class MainWindow : Gtk.Window
+{
+    public MainWindow() : base(Gtk.WindowType.Toplevel)
+    {
+        Build();
+    }
+
+    protected void OnDeleteEvent(object sender, DeleteEventArgs a)
+    {
+        Application.Quit();
+        a.RetVal = true;
+    }
+
+    protected void OnEntry1KeyReleaseEvent(object o, KeyReleaseEventArgs args)
+    {
+        if (args.Event.Key == Gdk.Key.Return)
+        {
+            changeText();
+        }
+    }
+
+    protected void changeText()
+    {
+        string name = entry1.Text;
+        label1.Text = $"Olá, {name}. Seja bem vindo!";
+        button1.Visible = entry1.Visible = false;
+
+        #pragma warning disable CS0612 // O tipo ou membro é obsoleto
+        label1.SetUposition(55, 80);
+        #pragma warning restore CS0612 // O tipo ou membro é obsoleto
+
+        Console.WriteLine(label1.Text);
+    }
+
+    protected void OnButton1Clicked(object sender, EventArgs e)
+    {
+        changeText();
+    }
+}
---
+++ C-Sharp-GTK2/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using Gtk;
+
+namespace CSharpGTK2
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            Application.Init();
+            MainWindow win = new MainWindow();
+            moveWindowToCenter(win);
+            win.Show();
+
+            Application.Run();
+        }
+
+        public static void moveWindowToCenter(Window window)
+        {
+            var screen = window.GdkWindow.Screen;
+            window.GdkWindow.GetSize(out int windowWidth, out int windowHeight);
+            int positionX = (screen.Width / 2) - (windowWidth / 2);
+            int positionY = (screen.Height / 2) - (windowHeight / 2);
+            window.Move(positionX, positionY);
+
+        }
+    }
+
+}
